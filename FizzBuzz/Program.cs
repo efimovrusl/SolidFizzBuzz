bfb48f775968e50f7cf10b7c1b85e92f2e6ec5ee
yefimov.ruslan <efimovrusl@gmail.com>
2023-12-08 04:55:23 +0000

    chore: Made a FizzBuzzService usage example

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SolidFizzBuzz.Fizzbuzz;
+using SolidFizzBuzz.Fizzbuzz.Rules;
+
+// Usage example
+List<IRule> rules = new() { new FizzRule(), new BuzzRule() };
+FizzbuzzService fizzbuzz = new (new ResultHandler(), rules.ToArray());
+
+fizzbuzz.Run(new Range(1, 16));
+
+// Wait
+Console.ReadKey();
